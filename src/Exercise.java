96800462d2e9e8d267a34ca9c325dbea2feb4339
Adey4k <150963491+Adey4k@users.noreply.github.com>
2025-02-25 20:54:19 +0200

    Update Exercise.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,21 @@
-//package domain;
+package domain;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static String getMd5Hash(String source) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(source.getBytes());
+        byte[] digest = md.digest();
+        return bytesToHex(digest);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
     }
 }
